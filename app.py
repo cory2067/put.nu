c64e92ab07f8f2a5109376cde0e0d41f77cc97cb
cory2067 <cor@mit.edu>
2018-03-02 02:51:14 -0500

    Add in old css.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, send_from_directory, redirect
+from flask import Flask, abort, send_from_directory, redirect, render_template
 import os
 
 app = Flask(__name__)
@@ -7,20 +7,20 @@ STORAGE_PATH = 'files'
 
 @app.route("/")
 def index():
-    return 'helo welcpome to put.nu'
-    
+    return render_template('index.html')
+
 @app.route("/<path>")
 def fetch(path):
     if not os.path.isfile(os.path.join(STORAGE_PATH, path)):
         return resolve_link(path) #if not a file, try to resolve as shortened link
 
-    return send_from_directory(STORAGE_PATH, path) 
+    return send_from_directory(STORAGE_PATH, path)
 
 def resolve_link(url): # link shortening
     shortened = {'zucc': 'https://facebook.com'} #use a real db for this
     if url in shortened:
         return redirect(shortened[url])
     abort(404)
-        
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000)
