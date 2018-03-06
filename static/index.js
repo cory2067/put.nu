a5d23b0768215be1e200ced64be32d7ac0964c8f
cory2067 <cor@mit.edu>
2018-03-06 17:33:51 -0500

    Add double click handler.

--- static/index.js
+++ static/index.js
@@ -1,3 +1,24 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+function read_file (f) {
+    if (!f.length) {
+        return;
+    }
+
+    // now do something with file
+}
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+
+    $(".bigtext, .smalltext").on("mousedown", function(e) {
+        // User shouldn't interact with(accidentally select) the text
+        e.preventDefault();
+    });
+
+    $("main").dblclick(function() {
+        $("#file-upload").click();
+    });
+
+    $("#file-upload").change(function() {
+        read_files(this.files);
+    })
+});
